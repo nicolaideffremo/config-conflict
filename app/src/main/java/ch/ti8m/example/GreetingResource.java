463060f752c6ea9c64339eb015feadcb344a10a8
Nicolai D'Effremo <nicolai.deffremo@ti8m.ch>
2026-02-13 11:07:21 +0100

    Clean ups

--- app/src/main/java/ch/ti8m/example/GreetingResource.java
+++ app/src/main/java/ch/ti8m/example/GreetingResource.java
@@ -1,20 +1,20 @@
 package ch.ti8m.example;
 
-import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/greetings")
 public class GreetingResource {
 
-    @Inject
-    GreetingConfig greetingConfig;
+    @ConfigProperty(name = "greeting.message")
+    private String message;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String greetings() {
-        return greetingConfig.message();
+        return message;
     }
 }
--- core/src/main/java/ch/ti8m/example/Log.java
+++ core/src/main/java/ch/ti8m/example/Log.java
@@ -1,4 +1,15 @@
 package ch.ti8m.example;
 
 public class Log {
+
+    private final String message;
+
+    Log(String message) {
+        this.message = message;
+    }
+
+    public String run() {
+        System.out.println(message);
+        return message;
+    }
 }
